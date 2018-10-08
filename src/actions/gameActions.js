0cdb3e827f5842ef59be5cf42649bf40d97a6417
Alex <0alexgutierrez@gmail.com>
2018-10-08 09:51:27 -0700

    added actions and reducers

---
+++ src/actions/gameActions.js
@@ -0,0 +1,23 @@
+export const SET_GUESS = 'SET_GUESS';
+export const setGuess = guess => ({
+  type: SET_GUESS,
+  guess
+});
+
+export const SET_FEEDBACK = 'SET_FEEDBACK';
+export const setFeedback = feedback => ({
+  type: SET_FEEDBACK,
+  feedback
+});
+
+export const SET_AURAL = 'SET_AURAL';
+export const setAural = aural => ({
+  type: SET_AURAL,
+  aural
+});
+
+export const SET_ANSWER = 'SET_ANSWER';
+export const setAnswer = answer => ({
+  type: SET_ANSWER,
+  answer
+});
---
+++ src/reducers/gameReducers.js
@@ -0,0 +1,34 @@
+import {
+  SET_GUESS,
+  SET_FEEDBACK,
+  SET_AURAL,
+  SET_ANSWER
+} from '../actions/gameActions';
+
+const initialState = {
+  guesses: [],
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.floor(Math.random() * 100) + 1
+};
+
+export const gameReducer = (state = initialState, action) => {
+  if (action.type === SET_GUESS) {
+    return Object.assign({}, state, {
+      guesses: [...state.guesses, action.guess]
+    });
+  } else if (action.type === SET_FEEDBACK) {
+    return Object.assign({}, state, {
+      feedback: action.feedback
+    });
+  } else if (action.type === SET_AURAL) {
+    return Object.assign({}, state, {
+      auralStatus: action.aural
+    });
+  } else if (action.type === SET_ANSWER) {
+    return Object.assign({}, state, {
+      correctAnswer: action.answer
+    });
+  }
+  return state;
+};
